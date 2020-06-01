25173b4c0ecd54e10b90c88807cc8e0481895d57
Abdulbasit Salah <abdullbasit99salah@gmail.com>
2020-06-01 10:14:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ let driver = {
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 }
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign(driver, { [key]: value });
+}
+destructivelyUpdateDriverWithKeyAndValue(driver, "address", "12 Broadway");
+
